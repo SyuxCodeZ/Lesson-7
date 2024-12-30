7ba98c37dc9789d5456aa73bac16dbbd39b60511
SyuxCodeZ <cuberdytm@gmail.com>
2024-12-30 15:44:27 +0600

    fgdfgdfgfdgfvdtgrfhf

---
+++ Activity1.py
@@ -0,0 +1,22 @@
+# section 1
+
+age = 12
+
+if (type(age) is int):
+    print ("it is an integer")
+
+else:
+    print ("it is not an integer")
+
+# section 2
+
+x = 420
+
+y= 420
+
+if (x is y):
+    print("x and y have the same value")
+
+y = 69
+if (x is not y):
+    print("x and y have different values")
